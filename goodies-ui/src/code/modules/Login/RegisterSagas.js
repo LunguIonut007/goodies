800ecb76d888f2f7b52efc109ed81c7f7acdc318
Ionut <lungu.ioan98@gmail.com>
2017-08-24 21:38:55 +0300

    register cause fix

--- goodies-ui/src/code/modules/Login/RegisterSagas.js
+++ goodies-ui/src/code/modules/Login/RegisterSagas.js
@@ -4,7 +4,8 @@ import RegisterActions from './RegisterRedux'
 export function * register(api, { payload: {username, email, name, password}, entityType }) {
 
     let data = { user: { email, username, password, name } }
-    data[entityType] = { 'address': '123 streen', phone: '00011122233', 'adminName': 'ME' }
+    let type = entityType === 'cause' ? 'organization' : 'donor'
+    data[type] = { 'address': '123 streen', phone: '00011122233', 'adminName': 'ME' }
     const response = yield call(api.register, data)
 
     if(response.status === 200) {
