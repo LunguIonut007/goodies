a6c2578287f0ad49322cd3cfd1da1e8c69175cb1
Ionut <lungu.ioan98@gmail.com>
2017-08-27 16:15:44 +0300

    login errors

--- goodies-mobile/App/Sagas/LoginSagas.js
+++ goodies-mobile/App/Sagas/LoginSagas.js
@@ -4,14 +4,17 @@ import { NavigationActions } from 'react-navigation'
 
 export const login = function * login (api, { username, password }) {
   const response = yield call(api.login, username, password)
-  console.log(response)
+
   if (response.status === 200) {
     let data = response.data
     data.entityType = data.entityType === 'DONOR' ? 'donator' : 'cause'
-    yield put(LoginActions.loginSuccess(data))
-    yield put(NavigationActions.navigate({routeName: 'Tab'}))
+    if (data.entityType === 'donator') yield put(LoginActions.loginFailure('Doar cauzele se pot loga'))
+    else {
+      yield put(LoginActions.loginSuccess(data))
+      yield put(NavigationActions.navigate({routeName: 'Tab'}))
+    }
   } else {
-    yield put(LoginActions.loginFailure('login failed'))
+    yield put(LoginActions.loginFailure('Email sau parola gresita'))
   }
 }
 
--- goodies-mobile/App/modules/Login/LoginScreen.js
+++ goodies-mobile/App/modules/Login/LoginScreen.js
@@ -4,23 +4,24 @@ import { View } from 'react-native'
 import { connect } from 'react-redux'
 import TextInput from './../../core/Form/TextInput'
 import LoginActions from './../../Redux/LoginRedux'
-import {Button} from 'react-native-elements'
+import {Button, FormValidationMessage} from 'react-native-elements'
 import styles from './LoginStyles'
 
 class LoginScreen extends Component {
   onSubmit = data => {
-    console.log(this.props.login)
-    // this.props.login(data.email, data.password)
-    this.props.login('copii@cauza.com', 'password')
+    console.log('data', data)
+    this.props.login(data.email, data.password)
   }
 
   render () {
-    const { handleSubmit } = this.props
+    const { handleSubmit, sagaError } = this.props
+    console.log(this.props)
     return (
       <View style={styles.view}>
         <View style={styles.container}>
           <Field name='email' label={'Email'} component={TextInput} />
           <Field name='password' label={'Parolă'} component={TextInput} />
+          {sagaError && <FormValidationMessage>{sagaError}</FormValidationMessage>}
           <Button containerViewStyle={styles.buttonContainer} buttonStyle={styles.button} onPress={handleSubmit(this.onSubmit)} title='Loghează-te' />
         </View>
       </View>
@@ -29,7 +30,7 @@ class LoginScreen extends Component {
 }
 
 export default connect(state => ({
-
+  sagaError: state.login.error
 }), {
   login: LoginActions.loginRequest
 })(reduxForm({
